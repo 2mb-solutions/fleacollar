95f830629128cb594978ac71f3bca030fb87b136
Storm Dragon <stormdragon2976@gmail.com>
2016-07-10 23:09:35 -0400

    Started work on adding email addresses.

--- fleacollar.sh
+++ fleacollar.sh
@@ -12,10 +12,19 @@ muttHome="$HOME/.mutt"
 # Functions
 add_email_address()
 {
-read -p "Please enter your email address: " emailAddress
-if [ -f "$emailAddress" ]; then
-read -p "This email address alread exists, would you like to remove it? " continue
-
+    read -p "Please enter your email address: " emailAddress
+    if ! [[ "$emailAddress" =~ .*@.*\..* ]]; then
+        read -p "this appears to be an invalid email address. Continue anyway? " continue
+        if [ "${continue^}" != "Y" ]; then
+            exit 0
+        fi
+    fi
+    if [ -f "$muttHome/$emailAddress" ]; then
+        read -p "This email address already exists. Overwrite the existing settings? " continue
+        if [ "${continue^}" != "Y" ]; then
+            exit 0
+        fi
+    fi
 }
 
 # This is the main loop of the program
